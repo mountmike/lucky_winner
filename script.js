a105265e4a4d8777f6e73a4afb77e506f7412674
Michael <tharratt.michael@gmail.com>
2023-02-21 17:12:07 +1100

    revising name list

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function letsSpin() {
 }
 
 let winnerOutput = document.querySelector("#luckywinner");
-let studentNames = ["akram mansour", "kevin utama", "amal prasad", "liang yu", "aung khant thiha", "maria valentina gottardello", "bilal foggi", "michael tharratt", "caleb love", "cicero teixeira", "mindy nam", "mohammed ali", "deepali patro", "nikki pham", "dimitri nossar", "simon ruc", "elmira alimohammadi", "stefanus lie", "falko kammel", "tales steganho pinto", "frances lozada", "tom nicholas", "henry rennell", "whoopi hutman",
+let studentNames = ["akram mansour", "kevin utama", "amal prasad", "liang yu", "aung khant thiha", "maria gottardello", "bilal foggi", "mike tharratt", "caleb love", "cicero teixeira", "mindy nam", "mohammed ali", "deepali patro", "nikki pham", "dimitri nossar", "simon ruc", "elmira alimohammadi", "stefanus lie", "falko kammel", "tales pinto", "frances lozada", "tom nicholas", "henry rennell", "whoopi hutman",
 "iffath", "zhenyu chen"];
 let colours = ["#ff1f1f", "#19e3cf", "#9e0bf3", "#15b600", "#1f26ff", "#ff5a5a", "#57fff1", "#ff9612"]
 
